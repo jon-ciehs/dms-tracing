89f0abd59af9a2520eee46d6ee97d3a9efdcaa2b
Mr Robot <blancorodriguez.jonathan@gmail.com>
2022-06-09 18:28:27 +0200

    first services

---
+++ configserver/src/test/java/tracing/dev/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tracing.dev.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ jobs/src/test/java/tracing/dev/jobs/JobsApplicationTests.java
@@ -0,0 +1,13 @@
+package tracing.dev.jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
